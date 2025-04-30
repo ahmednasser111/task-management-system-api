f8eb8bc77612b51822c555018c729d12ea557d7a
ahmednaser111 <ahmednaser7707@gmail.com>
2025-04-30 21:52:24 +0300

    try

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
-    await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to database.....");
   } catch (error) {
     console.log("Failed to connect to database.....", error.message);
